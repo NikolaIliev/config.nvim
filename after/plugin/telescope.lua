a3b5edb04f4e1ecc190827f061ca45c4b65c4bc0
Nikola Iliev <nikola.iliev93@gmail.com>
2023-04-18 14:27:22 +0300

    Switch <Esc> to call oldfiles instead of buffer (it actually does what I want)

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,13 +1,13 @@
 require('telescope').setup({
   defaults = {
-    path_display = { "truncate" }
-  }
+    path_display = { 'truncate' },
+  },
 })
 
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>fa', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
-vim.keymap.set('n', '<Esc>', builtin.buffers, {})
-vim.keymap.set('n', '<leader>ps', function ()
-	builtin.grep_string({ search = vim.fn.input("Grep > ") })
+vim.keymap.set('n', '<Esc>', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader>ps', function()
+  builtin.grep_string({ search = vim.fn.input('Grep > ') })
 end, {})
