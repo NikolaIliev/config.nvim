59b62250adb1b64bdeb261dbaf10a8f1bd301238
Nikola Iliev <nikola.iliev93@gmail.com>
2023-04-23 08:59:54 +0300

    Use ivy theme for all telescope builtins

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -5,21 +5,25 @@ require('telescope').setup({
 })
 
 local builtin = require('telescope.builtin')
-vim.keymap.set('n', '<leader>fa', builtin.find_files, {})
-vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
-vim.keymap.set('n', '<leader>ss', builtin.lsp_document_symbols, {})
-vim.keymap.set('n', '<leader>sa', builtin.lsp_workspace_symbols, {})
-vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
-vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
-vim.keymap.set('n', '<leader>gh', builtin.git_bcommits, {})
+local themes = require('telescope.themes')
 
-vim.keymap.set('n', '<Esc>', function()
-  builtin.buffers({ sort_lastused = true })
-end, {})
+local function telescope_builtin(fn, opts)
+  return function()
+    builtin[fn](themes.get_ivy(opts))
+  end
+end
 
-vim.keymap.set('n', '<leader>pd', function()
-  builtin.diagnostics({ severity = 'error' })
-end, {})
+vim.keymap.set('n', '<leader>fa', telescope_builtin('find_files'), {})
+vim.keymap.set('n', '<leader>ff', telescope_builtin('git_files'), {})
+vim.keymap.set('n', '<leader>ss', telescope_builtin('lsp_document_symbols'), {})
+vim.keymap.set('n', '<leader>sa', telescope_builtin('lsp_workspace_symbols'), {})
+vim.keymap.set('n', '<leader>gb', telescope_builtin('git_branches'), {})
+vim.keymap.set('n', '<leader>gs', telescope_builtin('git_status'), {})
+vim.keymap.set('n', '<leader>gh', telescope_builtin('git_bcommits'), {})
+
+vim.keymap.set('n', '<Esc>', telescope_builtin('buffers', { sort_lastused = true }), {})
+
+vim.keymap.set('n', '<leader>pd', telescope_builtin('diagnostics', { severity = 'error' }), {})
 
 vim.keymap.set('n', '<leader>ps', function()
   builtin.grep_string({ search = vim.fn.input('Grep > ') })
