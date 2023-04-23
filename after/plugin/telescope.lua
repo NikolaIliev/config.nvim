ced4d0fcfb7156816aee32eadc71c4ad5a47e293
Nikola Iliev <nikola.iliev93@gmail.com>
2023-04-23 11:03:44 +0300

    Telescope: remap navigation; Add functions to find libraries and files in current library

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,20 +1,130 @@
+local actions = require('telescope.actions')
+local builtin = require('telescope.builtin')
+local entry_display = require('telescope.pickers.entry_display')
+local path = require('plenary.path')
+local themes = require('telescope.themes')
+local theme = themes.get_ivy
+
 require('telescope').setup({
   defaults = {
     path_display = { 'truncate' },
+    mappings = {
+      i = {
+        ['<esc>'] = actions.close,
+        ['<C-j>'] = actions.move_selection_next,
+        ['<C-k>'] = actions.move_selection_previous,
+      },
+    },
   },
 })
 
-local builtin = require('telescope.builtin')
-local themes = require('telescope.themes')
-
 local function telescope_builtin(fn, opts)
   return function()
-    builtin[fn](themes.get_ivy(opts))
+    builtin[fn](theme(opts))
   end
 end
 
-vim.keymap.set('n', '<leader>fa', telescope_builtin('find_files'), {})
-vim.keymap.set('n', '<leader>ff', telescope_builtin('git_files'), {})
+-- Custom function for ignoring android and ios folders, and including hidden files
+local function find_files_ignore_android_ios()
+  builtin.find_files(theme({
+    find_command = {
+      'rg',
+      '--files',
+      '--hidden',
+      '--iglob',
+      '!apps/mobile/android/**',
+      '--iglob',
+      '!apps/mobile/ios/**',
+      '--iglob',
+      '!.git/**',
+    },
+  }))
+end
+
+-- Helper function to find the nearest parent directory containing a package.json file
+local function find_nearest_package_json_dir(start_dir)
+  local current_dir = path:new(start_dir)
+  while current_dir ~= nil do
+    local package_json_path = current_dir:joinpath('package.json')
+    if package_json_path:exists() then
+      return current_dir
+    end
+    current_dir = current_dir:parent()
+  end
+  return nil
+end
+
+-- Custom function for searching files within the current library
+local function find_files_in_current_library()
+  local current_file_dir = vim.fn.expand('%:p:h')
+  local library_root = find_nearest_package_json_dir(current_file_dir)
+
+  if library_root == nil then
+    print('No library with package.json found.')
+    return
+  end
+
+  local split_path = vim.split(library_root.filename, '/')
+  local library_name = split_path[#split_path]
+
+  print(library_name)
+
+  builtin.find_files(theme({
+    cwd = library_root.filename,
+    prompt_title = 'Files in ' .. library_name,
+  }))
+end
+
+local function find_git_repo_root(starting_path)
+  local current_path = path:new(starting_path)
+  local git_path = current_path:joinpath('.git')
+
+  while not git_path:exists() do
+    local parent_path = current_path:parent()
+    if not parent_path or parent_path.filename == current_path.filename then
+      return nil
+    end
+    current_path = parent_path
+    git_path = current_path:joinpath('.git')
+  end
+
+  return current_path
+end
+
+local function find_libraries()
+  local current_file_dir = vim.fn.expand('%:p:h')
+  local repo_root = find_git_repo_root(current_file_dir)
+
+  if repo_root == nil then
+    print('Git repository root not found.')
+    return
+  end
+
+  local displayer = entry_display.create({
+    separator = ' ',
+    items = { { width = 50 } },
+  })
+
+  local function make_display(entry)
+    local library_name = vim.fn.fnamemodify(entry.value, ':h:t')
+    return displayer({ library_name })
+  end
+
+  builtin.find_files({
+    prompt_title = 'Find Libraries',
+    cwd = repo_root.filename,
+    find_command = { 'rg', '--files', '--hidden', '--iglob', 'libs/**/tsconfig.json' },
+    entry_maker = function(entry, opts)
+      local original_maker = require('telescope.make_entry').gen_from_file(opts)
+      local original_entry = original_maker(entry)
+      original_entry.display = make_display
+      return original_entry
+    end,
+  })
+end
+
+vim.keymap.set('n', '<leader>fa', telescope_builtin('find_files', { hidden = true }), {})
+vim.keymap.set('n', '<leader>ff', find_files_ignore_android_ios, {})
 vim.keymap.set('n', '<leader>ss', telescope_builtin('lsp_document_symbols'), {})
 vim.keymap.set('n', '<leader>sa', telescope_builtin('lsp_workspace_symbols'), {})
 vim.keymap.set('n', '<leader>gb', telescope_builtin('git_branches'), {})
@@ -28,3 +138,6 @@ vim.keymap.set('n', '<leader>pd', telescope_builtin('diagnostics', { severity = 
 vim.keymap.set('n', '<leader>ps', function()
   builtin.grep_string({ search = vim.fn.input('Grep > ') })
 end, {})
+
+vim.keymap.set('n', '<leader>ll', find_libraries)
+vim.keymap.set('n', '<leader>fl', find_files_in_current_library)
