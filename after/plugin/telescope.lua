311eede979f53a0e0f382be4f9f3c2c915fdf5aa
Nikola Iliev <nikola.iliev93@gmail.com>
2023-04-23 08:45:18 +0300

    Telescope config improvements: Added mapping for git_branches, symbol search, diagnostics; Switched <Esc> to work with buffers with sort_lastused=true

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -7,7 +7,20 @@ require('telescope').setup({
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>fa', builtin.find_files, {})
 vim.keymap.set('n', '<leader>ff', builtin.git_files, {})
-vim.keymap.set('n', '<Esc>', builtin.oldfiles, {})
+vim.keymap.set('n', '<leader>ss', builtin.lsp_document_symbols, {})
+vim.keymap.set('n', '<leader>sa', builtin.lsp_workspace_symbols, {})
+vim.keymap.set('n', '<leader>gb', builtin.git_branches, {})
+vim.keymap.set('n', '<leader>gs', builtin.git_status, {})
+vim.keymap.set('n', '<leader>gh', builtin.git_bcommits, {})
+
+vim.keymap.set('n', '<Esc>', function()
+  builtin.buffers({ sort_lastused = true })
+end, {})
+
+vim.keymap.set('n', '<leader>pd', function()
+  builtin.diagnostics({ severity = 'error' })
+end, {})
+
 vim.keymap.set('n', '<leader>ps', function()
   builtin.grep_string({ search = vim.fn.input('Grep > ') })
 end, {})
