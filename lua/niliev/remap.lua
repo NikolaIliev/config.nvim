ec854ed118ca7eac61f94a56b3995db561049331
Nikola Iliev <nikola.iliev93@gmail.com>
2023-04-27 09:48:19 +0300

    Fix mind-boggling behaviour in terminal mode

--- lua/niliev/remap.lua
+++ lua/niliev/remap.lua
@@ -1,16 +1,20 @@
-vim.g.mapleader = " "
+vim.g.mapleader = ' '
 
-vim.keymap.set("n", "<leader>pv", ":Ex<CR>")
+vim.keymap.set('n', '<leader>pv', ':Ex<CR>')
 
 -- more ergonomic navigation over buffer splits
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
+vim.keymap.set('n', '<C-h>', '<C-w>h')
+vim.keymap.set('n', '<C-j>', '<C-w>j')
+vim.keymap.set('n', '<C-k>', '<C-w>k')
+vim.keymap.set('n', '<C-l>', '<C-w>l')
 
 -- in terminal mode, more ergonomic way to go to normal mode
-vim.keymap.set("t", "<C-w>", "<C-\\><C-n>")
+vim.keymap.set('t', '<C-w>', '<C-\\><C-n>')
 
 -- remap scrolling
-vim.keymap.set("n", "<C-f>", "L5j")
-vim.keymap.set("n", "<C-b>", "H5k")
+vim.keymap.set('n', '<C-f>', 'L5j')
+vim.keymap.set('n', '<C-b>', 'H5k')
+
+-- for some reason in terminal mode shift+space outputs escape
+-- this keymap restores the sane behavior
+vim.keymap.set('t', '<S-Space>', '<Space>')
