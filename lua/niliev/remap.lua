1ebb5989ceaf4295641e3989084342c33d5e0edf
Nikola Iliev <nikola.iliev93@gmail.com>
2023-04-27 09:50:55 +0300

    Group remaps by mode

--- lua/niliev/remap.lua
+++ lua/niliev/remap.lua
@@ -1,5 +1,6 @@
 vim.g.mapleader = ' '
 
+-------- Normal mode --------
 vim.keymap.set('n', '<leader>pv', ':Ex<CR>')
 
 -- more ergonomic navigation over buffer splits
@@ -8,13 +9,15 @@ vim.keymap.set('n', '<C-j>', '<C-w>j')
 vim.keymap.set('n', '<C-k>', '<C-w>k')
 vim.keymap.set('n', '<C-l>', '<C-w>l')
 
--- in terminal mode, more ergonomic way to go to normal mode
-vim.keymap.set('t', '<C-w>', '<C-\\><C-n>')
-
 -- remap scrolling
 vim.keymap.set('n', '<C-f>', 'L5j')
 vim.keymap.set('n', '<C-b>', 'H5k')
 
+-------- Terminal mode --------
+
 -- for some reason in terminal mode shift+space outputs escape
 -- this keymap restores the sane behavior
 vim.keymap.set('t', '<S-Space>', '<Space>')
+
+-- in terminal mode, more ergonomic way to go to normal mode
+vim.keymap.set('t', '<C-w>', '<C-\\><C-n>')
