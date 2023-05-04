57dcdf44356df3ab590ef847234e8b9daa4ae718
Nikola Iliev <nikola.iliev93@gmail.com>
2023-05-04 12:54:00 +0300

    Some Neovide settings

--- lua/niliev/init.lua
+++ lua/niliev/init.lua
@@ -1,3 +1,4 @@
-require("niliev.remap")
-require("niliev.packer")
-require("niliev.set")
+require('niliev.remap')
+require('niliev.packer')
+require('niliev.set')
+require('niliev.neovide')
---
+++ lua/niliev/neovide.lua
@@ -0,0 +1,7 @@
+if vim.g.neovide then
+  vim.g.neovide_floating_blur_amount_x = 2.0
+  vim.g.neovide_floating_blur_amount_y = 2.0
+  vim.g.neovide_transparency = 0.9
+  vim.g.neovide_refresh_rate = 120
+  vim.g.neovide_cursor_vfx_mode = 'railgun'
+end
