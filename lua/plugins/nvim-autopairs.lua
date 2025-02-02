aa01f2f827af730956c1471223f976819681d4e9
Nikola Iliev <nikola.iliev93@gmail.com>
2025-02-02 20:41:57 +0200

    add autopair rule for <>

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -2,4 +2,12 @@ return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
   opts = {},
+  config = function(_, opts)
+    local npairs = require('nvim-autopairs')
+    npairs.setup()
+
+    local Rule = require('nvim-autopairs.rule')
+
+    npairs.add_rule(Rule('<', '>'))
+  end,
 }
