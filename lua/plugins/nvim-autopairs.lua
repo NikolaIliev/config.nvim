7a2d6b34d813db6a2ca1ad3e2f9cb19723365010
Nikola Iliev <nikola.iliev93@gmail.com>
2025-02-03 16:53:48 +0200

    Revert "add autopair rule for <>"
    This reverts commit aa01f2f827af730956c1471223f976819681d4e9.

--- lua/plugins/nvim-autopairs.lua
+++ lua/plugins/nvim-autopairs.lua
@@ -2,12 +2,4 @@ return {
   'windwp/nvim-autopairs',
   event = 'InsertEnter',
   opts = {},
-  config = function(_, opts)
-    local npairs = require('nvim-autopairs')
-    npairs.setup()
-
-    local Rule = require('nvim-autopairs.rule')
-
-    npairs.add_rule(Rule('<', '>'))
-  end,
 }
